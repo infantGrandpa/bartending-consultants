337b8ee444299ca849859574c1227dca4ce2e3a1
Brett Abraham <babraham341@gmail.com>
2025-10-18 16:59:00 -0400

    Save and clear ElevenLabs API Key as well

--- api-key-handler.js
+++ api-key-handler.js
@@ -1,16 +1,19 @@
 ﻿import {addMessageToLog, hideMessageArea, showMessageArea} from "./element-controller.js";
 
 export let openAiApiKey = localStorage.getItem('openai_api_key');
+export let elevenLabsApiKey = localStorage.getItem('elevenlabs_api_key')
 
 const apiKeySetup = document.getElementById('apiKeySetup');
-const apiKeyInput = document.getElementById('apiKeyInput');
+const openaiApiKeyInput = document.getElementById('openaiApiKeyInput');
+const elevenLabsApiKeyInput = document.getElementById('elevenLabsApiKeyInput');
 
 const clearApiKeysButton = document.getElementById('clearApiKeysButton');
 const saveApiKeysButton = document.getElementById('saveApiKeysButton');
 
 function checkApiKeyStatus() {
-    if (!openAiApiKey) {
+    if (!openAiApiKey || !elevenLabsApiKey) {
         apiKeySetup.classList.remove('hidden');
+        clearApiKeysButton.classList.add('hidden');
         hideMessageArea();
     } else {
         apiKeySetup.classList.add('hidden');
@@ -21,15 +24,44 @@ function checkApiKeyStatus() {
 
 checkApiKeyStatus();
 
+function getOpenAiKeyFromInput() {
+    const apiKeyValue = openaiApiKeyInput.value.trim();
+    if (!apiKeyValue) {
+        alert('Please enter an OpenAI API key.');
+        return false;
+    }
+
+    localStorage.setItem('openai_api_key', apiKeyValue);
+    openAiApiKey = apiKeyValue;
+
+    return true;
+}
+
+function getElevenLabsKeyFromInput() {
+    const apiKeyValue = elevenLabsApiKeyInput.value.trim();
+    if (!apiKeyValue) {
+        alert('Please enter an ElevenLabs API key.');
+        return false;
+    }
+
+    localStorage.setItem('elevenlabs_api_key', apiKeyValue);
+    elevenLabsApiKey = apiKeyValue;
+    return true;
+}
+
 saveApiKeysButton.addEventListener('click', () => {
-    const apiKey = apiKeyInput.value.trim();
-    if (!apiKey) {
-        alert('Please enter an API key');
+    let success = getOpenAiKeyFromInput();
+    if (success) {
+        success = getElevenLabsKeyFromInput();
+    }
+
+    if (!success) {
         return;
     }
-    localStorage.setItem('openai_api_key', apiKey);
-    openAiApiKey = apiKey;
-    apiKeyInput.value = '';
+
+    openaiApiKeyInput.value = '';
+    elevenLabsApiKeyInput.value = '';
+
     checkApiKeyStatus();
     addMessageToLog('System', 'API key saved successfully!');
 });
@@ -38,6 +70,10 @@ clearApiKeysButton.addEventListener('click', () => {
     if (confirm('Are you sure you want to clear your stored API key?')) {
         localStorage.removeItem('openai_api_key');
         openAiApiKey = null;
+
+        localStorage.removeItem('elevenlabs_api_key');
+        elevenLabsApiKey = null;
+
         checkApiKeyStatus();
         console.log('API Keys cleared.')
     }
