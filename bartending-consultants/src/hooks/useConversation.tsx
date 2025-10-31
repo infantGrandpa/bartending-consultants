b96e8b7873785feda99770cc9166a4fe5f5d5bd2
Brett Abraham <babraham341@gmail.com>
2025-10-31 17:13:20 -0400

    Setup basic useConversation hook

---
+++ bartending-consultants/src/hooks/useConversation.tsx
@@ -0,0 +1,37 @@
+﻿import {useState} from "react";
+import {getSystemPrompt} from "../utils/personalities.ts";
+import {addResponseToConversation, createConversation} from "../api/openai.ts";
+import {stripMarkdownFromString} from "../utils/utils.ts";
+
+
+export function useConversation() {
+    const [conversationId, setConversationId] = useState<string>("");
+    const [personality, setPersonality] = useState<any | null>(null);
+    const [messages, setMessages] = useState<{ sender: string; text: string }[]>([]);
+    const [drink, setDrink] = useState<any | null>(null);
+
+    //TODO: Change personality
+
+    const sendMessage = async (message: string)=> {
+        if (!personality) throw new Error("No personality selected.");
+
+        if (!conversationId) {
+            console.log(`No conversation ID found. Creating a new conversation...`);
+            const systemPrompt = getSystemPrompt(personality);
+            const newId: string = await createConversation(systemPrompt, message, personality.key);
+            setConversationId(newId);
+        }
+
+        setMessages((prev) => [...prev, { sender: "You", text: message }]);
+        const responseString = await addResponseToConversation(message, conversationId);
+        const response = JSON.parse(responseString);
+
+        const reply = stripMarkdownFromString(response.message);
+        setMessages((prev) => [...prev, { sender: personality.displayName, text: reply }]);
+        setDrink(response.drink);
+
+        //TODO: Speak text
+    }
+
+    return { personality, sendMessage, messages, drink };
+}
