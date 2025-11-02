c84e45b08d0eac7aff45cb86ce06992d5c743eb5
Brett Abraham <babraham341@gmail.com>
2025-11-02 10:48:53 -0500

    Setup grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import Sidebar from "./components/Sidebar";
 import MessagingPanel from "./components/MessagingPanel";
 import {ApiKeyProvider} from "./hooks/useApiKeys.tsx";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import {Grid, Theme} from "@radix-ui/themes";
 
 export default function App() {
     return (
         <Theme accentColor="violet" appearance="dark" grayColor="mauve" radius="large" scaling="110%">
             <ApiKeyProvider>
-                <div className="grid grid-cols-3 min-h-screen">
+                <Grid columns="3" gap="3">
                     <Sidebar/>
                     <MessagingPanel/>
-                </div>
+                </Grid>
             </ApiKeyProvider>
         </Theme>
     );
