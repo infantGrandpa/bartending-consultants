c262fe6bf2d25ef7de0fa51cd1555c31ea9e4007
Brett Abraham <babraham341@gmail.com>
2025-11-02 10:35:55 -0500

    Setup Radix with Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-// src/components/App.tsx
 import Sidebar from "./components/Sidebar";
 import MessagingPanel from "./components/MessagingPanel";
 import {ApiKeyProvider} from "./hooks/useApiKeys.tsx";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 export default function App() {
     return (
-        <ApiKeyProvider>
-            <div className="grid grid-cols-3 min-h-screen">
-                <Sidebar />
-                <MessagingPanel />
-            </div>
-        </ApiKeyProvider>
+        <Theme>
+            <ApiKeyProvider>
+                <div className="grid grid-cols-3 min-h-screen">
+                    <Sidebar/>
+                    <MessagingPanel/>
+                </div>
+            </ApiKeyProvider>
+        </Theme>
     );
 }
