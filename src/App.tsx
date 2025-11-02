83c98011b9f5618505dee8dcfc4ec4b37661e8e0
Brett Abraham <babraham341@gmail.com>
2025-11-02 10:57:54 -0500

    Set panels to translucent

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import {Grid, Theme} from "@radix-ui/themes";
 
 export default function App() {
     return (
-        <Theme accentColor="violet" appearance="dark" grayColor="mauve" radius="large" scaling="110%">
+        <Theme accentColor="violet" appearance="dark" grayColor="mauve" radius="large" scaling="110%"
+               panelBackground="translucent">
             <ApiKeyProvider>
                 <Grid columns="3" gap="3">
                     <Sidebar/>
