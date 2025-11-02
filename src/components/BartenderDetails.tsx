1964891066243b94a66edd10802b7a2a5034a1af
Brett Abraham <babraham341@gmail.com>
2025-11-02 11:50:57 -0500

    Setup BartenderDetails.tsx

--- src/components/BartenderDetails.tsx
+++ src/components/BartenderDetails.tsx
@@ -1,15 +1,29 @@
-﻿import {useState} from "react";
+﻿import {Card, Heading, Inset} from "@radix-ui/themes";
+import {usePersonality} from "../providers/PersonalityProvider.tsx";
 
 export default function BartenderDetails() {
-    const [imageUrl] = useState(
-        "https://placehold.co/574x861/030712/white?text=Choose+a+Bartender&font=roboto"
-    );
+    const {selectedPersonality} = usePersonality();
+
+    const placeholderImageUrl: string = "https://placehold.co/574x861/030712/white?text=Choose+a+Bartender&font=roboto"
+    const imageUrl: string = selectedPersonality?.imagePath ?? placeholderImageUrl;
+
 
     return (
-        <img
-            src={imageUrl}
-            alt="Bartender"
-            className="w-full max-h-[1000px] object-contain"
-        />
+        <Card>
+            <Inset clip="padding-box" side="top" pb="current">
+                <img
+                    src={imageUrl}
+                    alt={`${selectedPersonality?.displayName} Bartender`}
+                    style={{
+                        display: "block",
+                        objectFit: "cover",
+                        width: "100%",
+                        height: 140,
+                        backgroundColor: "var(--gray-5)",
+                    }}
+                />
+            </Inset>
+            <Heading as="h3" size="4">{selectedPersonality?.displayName}</Heading>
+        </Card>
     );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,13 @@
 ﻿import PersonalitySelection from "./PersonalitySelection";
 import ApiKeysPanel from "./ApiKeys";
 import {Flex} from "@radix-ui/themes";
+import BartenderDetails from "./BartenderDetails.tsx";
 
 export default function Sidebar() {
     return (
         <Flex direction="column" m="4">
             <PersonalitySelection />
+            <BartenderDetails />
             <ApiKeysPanel />
         </Flex>
     );
