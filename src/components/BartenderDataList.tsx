26a43cf4b4fe85e1f757cb08ad46eac32385edae
Brett Abraham <babraham341@gmail.com>
2025-11-05 16:31:57 -0500

    Add bartender profile details to datalist

--- src/components/BartenderDataList.tsx
+++ src/components/BartenderDataList.tsx
@@ -4,13 +4,26 @@ import {useBartender} from "../providers/BartenderProvider.tsx";
 export default function BartenderDataList() {
     const {selectedBartender} = useBartender();
 
+    const profile = selectedBartender?.profile;
+
     return (
         <DataList.Root>
             <DataList.Item align="center">
                 <DataList.Label minWidth="88px">Name</DataList.Label>
-                <DataList.Value>{selectedBartender?.profile.firstName}</DataList.Value>
+                <DataList.Value>{profile?.firstName}</DataList.Value>
             </DataList.Item>
-
+            <DataList.Item align="center">
+                <DataList.Label minWidth="88px">Age</DataList.Label>
+                <DataList.Value>{profile?.age}</DataList.Value>
+            </DataList.Item>
+            <DataList.Item align="center">
+                <DataList.Label minWidth="88px">Hometown</DataList.Label>
+                <DataList.Value>{profile?.hometown}</DataList.Value>
+            </DataList.Item>
+            <DataList.Item align="center">
+                <DataList.Label minWidth="88px">Pronouns</DataList.Label>
+                <DataList.Value>{profile?.pronouns}</DataList.Value>
+            </DataList.Item>
         </DataList.Root>
     )
 }
