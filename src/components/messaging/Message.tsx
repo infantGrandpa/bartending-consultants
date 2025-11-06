124e512e8c797c43c47b2a7e78036e10e18fc808
Brett Abraham <babraham341@gmail.com>
2025-11-06 16:45:43 -0500

    Add message component

---
+++ src/components/messaging/Message.tsx
@@ -0,0 +1,16 @@
+﻿import {Card, Text} from "@radix-ui/themes";
+
+
+interface Props {
+    message: string;
+    sender: string;
+}
+
+export default function Message({message, sender}: Props) {
+    return (
+        <Card>
+            <Text as="div" size="2" weight="bold">{sender}</Text>
+            <Text as="div" size="2" color="gray">{message}</Text>
+        </Card>
+    );
+}
