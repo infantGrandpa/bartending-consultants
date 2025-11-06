fe0faa86410766bae3401847257d7bdc914b052a
Brett Abraham <babraham341@gmail.com>
2025-11-06 11:34:58 -0500

    Handle message input and sending logic

--- src/components/messaging/MessageInput.tsx
+++ src/components/messaging/MessageInput.tsx
@@ -1,7 +1,26 @@
 ﻿import {Flex, IconButton, TextArea} from "@radix-ui/themes";
+import {useState} from "react";
 
-export default function MessageInput() {
-    //TODO: Setup messages to work.
+interface Props {
+    onSendMessage: (message: string) => Promise<void>;
+}
+
+export default function MessageInput({onSendMessage}: Props) {
+    const [message, setMessage] = useState<string>("");
+
+    const handleSendButtonClick = async () => {
+        const trimmedMessage = message.trim();
+
+        if (trimmedMessage === "") {
+            return;
+        }
+
+        console.log(`Message: ${trimmedMessage}`)
+        //TODO: await onSendMessage(trimmedMessage);
+
+        setMessage("");
+    }
+
     //TODO: Allow speech to text dictation
 
     return (
@@ -12,13 +31,24 @@ export default function MessageInput() {
             color: "var(--gray-12)",
             borderRadius: "var(--radius-2)"
         }}>
-            <TextArea placeholder="Type your message here..." variant="soft" resize="vertical" style={{
-                backgroundColor: "initial",
-                flexGrow: "1",
-                minHeight: "96.8px"
-            }}></TextArea>
+            <TextArea
+                placeholder="Type your message here..."
+                variant="soft"
+                resize="vertical"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                style={{
+                    backgroundColor: "initial",
+                    flexGrow: "1",
+                    minHeight: "96.8px"
+                }}
+            />
             <Flex direction="column" m="2" gap="2" justify="end">
-                <IconButton variant="solid">
+                <IconButton
+                    variant="solid"
+                    onClick={handleSendButtonClick}
+                    disabled={message.trim().length === 0}
+                >
                     <i className="fa-solid fa-paper-plane"></i>
                 </IconButton>
                 <IconButton variant="outline">
