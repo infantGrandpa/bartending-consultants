3e7fe5ba842df06504f3bc415ff80d5dfc74faa0
Brett Abraham <babraham341@gmail.com>
2025-11-10 17:03:57 -0500

    Add isDevMode control

--- src/components/settings/DevControls.tsx
+++ src/components/settings/DevControls.tsx
@@ -1,8 +1,22 @@
-﻿import {Text} from "@radix-ui/themes";
+﻿import {Flex, Switch, Text} from "@radix-ui/themes";
+import {useState} from "react";
 
 
 export default function DevControls() {
+    const [isDevMode, setIsDevMode] = useState<boolean>(false);
+
     return (
-        <Text>Developer Tab!</Text>
+        <Flex direction="column" gap="3" p="3">
+            <Text as="label" size="3">
+                <Flex gap="2">
+                    <Switch
+                        size="2"
+                        checked={isDevMode}
+                        onClick={() => setIsDevMode(!isDevMode)}
+                    />
+                    Dev Mode
+                </Flex>
+            </Text>
+        </Flex>
     )
 }
