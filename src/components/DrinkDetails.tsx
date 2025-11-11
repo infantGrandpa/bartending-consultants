4e02ffcfd48bbbb009549c3ba9f8d8c33e58a983
Brett Abraham <babraham341@gmail.com>
2025-11-11 11:37:01 -0500

    Show most recent drink in sidebar

--- src/components/DrinkDetails.tsx
+++ src/components/DrinkDetails.tsx
@@ -1,8 +1,34 @@
-﻿import {Text} from "@radix-ui/themes";
+﻿import {Card, Heading} from "@radix-ui/themes";
+import {useConversation} from "../providers/ConversationProvider.tsx";
+import type {Drink, Ingredient} from "../types/drinks.ts";
 
 
 export default function DrinkDetails() {
+    const {getMostRecentDrink} = useConversation();
+
+    const currentDrink: Drink | undefined = getMostRecentDrink();
+
+    if (!currentDrink) {
+        return;
+    }
+
     return (
-        <Text>DrinkDetails</Text>
+        <Card style={{
+            width: "100%"
+        }}>
+            <Heading as="h3" size="4" mb="2">{currentDrink.name}</Heading>
+            <Heading as="h4" size="3">Ingredients</Heading>
+            <ul>
+                {currentDrink.ingredients.map((thisIngredient: Ingredient, index: number) => (
+                    <li key={index}>{thisIngredient.amount} {thisIngredient.ingredient}</li>
+                ))}
+            </ul>
+            <Heading as="h4" size="3">Instructions</Heading>
+            <ol>
+                {currentDrink.instructions.map((thisStep: string, index: number) => (
+                    <li key={index}>{thisStep}</li>
+                ))}
+            </ol>
+        </Card>
     )
 }
