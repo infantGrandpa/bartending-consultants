7f228bfbc5414e6e15520778094cc9682e8fa976
Brett Abraham <babraham341@gmail.com>
2025-11-11 10:43:22 -0500

    Create ConversationProvider.tsx

---
+++ src/providers/ConversationProvider.tsx
@@ -0,0 +1,54 @@
+﻿import {createContext, type ReactNode, useContext, useState} from "react";
+import type {Conversation, Message} from "../types/conversations.ts";
+
+interface ConversationContextValue {
+    conversation: Conversation;
+    setConversationId: (conversationId: string) => void;
+    addMessage: (message: Message) => void;
+    clearConversation: () => void;
+}
+
+const ConversationContext= createContext<ConversationContextValue | undefined>(undefined);
+
+const defaultConversation: Conversation = {
+    conversationId: "",
+    messages: []
+}
+
+export function ConversationProvider({children}: { children: ReactNode }) {
+    const [conversation, setConversation] = useState<Conversation>(defaultConversation);
+
+    const setConversationId = (conversationId: string) => {
+        setConversation(previousConversation => ({...previousConversation, conversationId}))
+    };
+
+    const addMessage = (message: Message) => {
+        setConversation(previousConversation => ({
+            ...previousConversation,
+            messages: [...previousConversation.messages, message]
+        }));
+    };
+
+    const clearConversation = () => {
+        setConversation(defaultConversation);
+    }
+
+    return (
+        <ConversationContext.Provider value={{
+            conversation,
+            setConversationId,
+            addMessage,
+            clearConversation
+        }}>
+            {children}
+        </ConversationContext.Provider>
+    )
+}
+
+export function useConversation() {
+    const context = useContext(ConversationContext);
+    if (context === undefined) {
+        throw new Error('useConversation must be used within a ConversationProvider.')
+    }
+    return context;
+}
