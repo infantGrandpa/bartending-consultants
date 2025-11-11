19819d0444b5eadcdb8a3b4ed7d53b10aaa41913
Brett Abraham <babraham341@gmail.com>
2025-11-11 10:32:15 -0500

    Add optional drink to Message type

--- src/types/conversations.ts
+++ src/types/conversations.ts
@@ -1,7 +1,10 @@
-﻿export interface Message {
+﻿import type {Drink} from "./drinks.ts";
+
+export interface Message {
     sender: string;
     content: string;
     senderIsUser: boolean;
+    drink?: Drink;
 }
 
 export interface ConversationHistory {
