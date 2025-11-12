bab32f0829a674ed8467d72c7ccaed86f750db5c
Brett Abraham <babraham341@gmail.com>
2025-11-12 13:10:13 -0500

    Setup Speech To Text with existing file

--- src/components/messaging/SpeechToTextInput.tsx
+++ src/components/messaging/SpeechToTextInput.tsx
@@ -1,14 +1,46 @@
-﻿import {IconButton} from "@radix-ui/themes";
+﻿import {ResultReason, SpeechConfig, AudioConfig, SpeechRecognizer} from "microsoft-cognitiveservices-speech-sdk";
 
 
 export default function SpeechToTextInput() {
-    const handleClick = () => {
-        console.log("Starting Dictation (not really)...")
+    const speechKey: string = "speech-key"
+    const endpoint: URL = new URL("http://endpoint-url.com");
+
+    async function fileChange(event) {
+        const audioFile = event.target.files[0];
+        console.log(audioFile);
+        const fileInfo = audioFile.name + ` size=${audioFile.size} bytes `;
+
+        console.log(fileInfo);
+
+        const speechConfig: SpeechConfig = SpeechConfig.fromEndpoint(endpoint, speechKey);
+        speechConfig.speechRecognitionLanguage = 'en-US';
+
+        const audioConfig = AudioConfig.fromWavFileInput(audioFile);
+        const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        recognizer.recognizeOnceAsync(result => {
+            let text;
+            if (result.reason === ResultReason.RecognizedSpeech) {
+                text = `RECOGNIZED: Text=${result.text}`
+            } else {
+                text = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+            }
+
+            console.log(fileInfo + text);
+        });
     }
 
+
     return (
-        <IconButton variant="outline" onClick={handleClick}>
-            <i className="fa-solid fa-microphone"></i>
-        </IconButton>
+        <div className="mt-2">
+            <label htmlFor="audio-file"><i className="fas fa-file-audio fa-lg mr-2"></i></label>
+            <input
+                type="file"
+                id="audio-file"
+                onChange={(e) => fileChange(e)}
+                style={{display: "none"}}
+            />
+            Convert speech to text from an audio file.
+        </div>
     )
 }
