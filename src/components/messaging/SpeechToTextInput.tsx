c7e22e580ea29abf70fbd3c3afbaa17bad6fdd35
Brett Abraham <babraham341@gmail.com>
2025-11-12 13:18:29 -0500

    Clean up SpeechToTextInput.tsx

--- src/components/messaging/SpeechToTextInput.tsx
+++ src/components/messaging/SpeechToTextInput.tsx
@@ -1,29 +1,35 @@
-﻿import {ResultReason, SpeechConfig, AudioConfig, SpeechRecognizer} from "microsoft-cognitiveservices-speech-sdk";
+﻿import {ResultReason, SpeechConfig, AudioConfig, SpeechRecognizer, SpeechRecognitionResult} from "microsoft-cognitiveservices-speech-sdk";
+import type { ChangeEvent } from "react";
 
 
 export default function SpeechToTextInput() {
     const speechKey: string = "speech-key"
     const endpoint: URL = new URL("http://endpoint-url.com");
 
-    async function fileChange(event) {
-        const audioFile = event.target.files[0];
-        console.log(audioFile);
-        const fileInfo = audioFile.name + ` size=${audioFile.size} bytes `;
+    async function fileChange(event: ChangeEvent<HTMLInputElement>) {
+        const audioFile: File | null = event.target.files?.[0] ?? null;
+        if (!audioFile) {
+            console.log("No audio file selected.");
+            return;
+        }
 
-        console.log(fileInfo);
+        console.log(audioFile);
+        const fileInfo: string = audioFile.name + ` size=${audioFile.size} bytes `;
+
+        console.log(`Selected File: ${fileInfo}`);
 
         const speechConfig: SpeechConfig = SpeechConfig.fromEndpoint(endpoint, speechKey);
         speechConfig.speechRecognitionLanguage = 'en-US';
 
-        const audioConfig = AudioConfig.fromWavFileInput(audioFile);
+        const audioConfig: AudioConfig = AudioConfig.fromWavFileInput(audioFile);
         const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
-        recognizer.recognizeOnceAsync(result => {
+        recognizer.recognizeOnceAsync((result: SpeechRecognitionResult) => {
             let text;
             if (result.reason === ResultReason.RecognizedSpeech) {
                 text = `RECOGNIZED: Text=${result.text}`
             } else {
-                text = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+                text = 'ERROR: Speech was cancelled or could not be recognized. Ensure you selected a WAV file.';
             }
 
             console.log(fileInfo + text);
@@ -33,11 +39,11 @@ export default function SpeechToTextInput() {
 
     return (
         <div className="mt-2">
-            <label htmlFor="audio-file"><i className="fas fa-file-audio fa-lg mr-2"></i></label>
+            <label htmlFor="audio-file"><i className="fa-solid fa-file-audio"></i></label>
             <input
                 type="file"
                 id="audio-file"
-                onChange={(e) => fileChange(e)}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => fileChange(e)}
                 style={{display: "none"}}
             />
             Convert speech to text from an audio file.
