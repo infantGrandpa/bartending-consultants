d8c567a3caff0d7dce336272ce7cc89846f54cf3
Brett Abraham <babraham341@gmail.com>
2025-11-17 17:05:11 -0500

    Remove sttFromFile

--- src/components/messaging/SpeechToTextInput.tsx
+++ src/components/messaging/SpeechToTextInput.tsx
@@ -2,12 +2,11 @@
     AudioConfig, CancellationReason,
     ResultReason,
     SpeechConfig,
-    SpeechRecognitionResult,
     SpeechRecognizer
 } from "microsoft-cognitiveservices-speech-sdk";
-import {type ChangeEvent, type RefObject, useRef, useState} from "react";
+import {type RefObject, useRef, useState} from "react";
 import {useApiKeys} from "../../providers/ApiKeyProvider.tsx";
-import {Button, Flex} from "@radix-ui/themes";
+import {Flex, IconButton} from "@radix-ui/themes";
 
 // A lot of this code is modified from: https://github.com/Azure-Samples/AzureSpeechReactSample/tree/main
 
@@ -23,23 +22,6 @@ export default function SpeechToTextInput() {
         return speechConfig;
     }
 
-    async function recognizeSpeechOnce(audioConfig: AudioConfig) {
-        const speechConfig = getSpeechConfig();
-        const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
-
-        return new Promise<string>((resolve, reject) => {
-            recognizer.recognizeOnceAsync((result: SpeechRecognitionResult) => {
-                    if (result.reason === ResultReason.RecognizedSpeech) {
-                        resolve(result.text);
-                    } else {
-                        resolve('ERROR: Speech was cancelled or could not be recognized.')
-                    }
-                }, (error) => {
-                    reject(error);
-                }
-            )
-        });
-    }
 
     async function startContinuousSttFromMic() {
         const audioConfig: AudioConfig = AudioConfig.fromDefaultMicrophoneInput();
@@ -95,43 +77,13 @@ export default function SpeechToTextInput() {
         }
     }
 
-
-    async function fileChange(event: ChangeEvent<HTMLInputElement>) {
-        const audioFile: File | null = event.target.files?.[0] ?? null;
-        if (!audioFile) {
-            console.log("No audio file selected.");
-            return;
-        }
-
-        console.log(audioFile);
-        const fileInfo: string = audioFile.name + ` size=${audioFile.size} bytes `;
-
-        console.log(`Selected File: ${fileInfo}`);
-
-        if (audioFile.type !== 'audio/wav') {
-            throw new Error("File must be a .wav Audio file.")
-        }
-
-        const audioConfig: AudioConfig = AudioConfig.fromWavFileInput(audioFile);
-        const text: string = await recognizeSpeechOnce(audioConfig)
-        console.log(`${fileInfo}: ${text}`);
-    }
-
-
     return (
         <Flex gap="3">
-            <label htmlFor="audio-file"><i className="fa-regular fa-file-audio"></i></label>
-            <input
-                type="file"
-                id="audio-file"
-                onChange={(e: ChangeEvent<HTMLInputElement>) => fileChange(e)}
-                style={{display: "none"}}
-            />
-            <Button
+            <IconButton
                 variant="outline"
                 onClick={() => isRecognizing ? stopContinuousSttFromMic() : startContinuousSttFromMic()}>
                 <i className={isRecognizing ? "fa-solid fa-microphone-slash" : "fa-solid fa-microphone"}></i>
-            </Button>
+            </IconButton>
         </Flex>
     )
 }
