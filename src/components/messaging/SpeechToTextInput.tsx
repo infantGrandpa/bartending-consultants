015536b314f4e9b6ae4f24e89aaa8d612c07914e
Brett Abraham <babraham341@gmail.com>
2025-11-18 13:51:13 -0500

    Add dialog for Speech to text

--- src/components/messaging/SpeechToTextInput.tsx
+++ src/components/messaging/SpeechToTextInput.tsx
@@ -8,7 +8,7 @@
 } from "microsoft-cognitiveservices-speech-sdk";
 import {type RefObject, useRef, useState} from "react";
 import {useApiKeys} from "../../providers/ApiKeyProvider.tsx";
-import {Flex, IconButton} from "@radix-ui/themes";
+import {AlertDialog, Button, Flex, IconButton, Text} from "@radix-ui/themes";
 import {useDevSettings} from "../../providers/DevSettingsProvider.tsx";
 
 // A lot of this code is modified from: https://github.com/Azure-Samples/AzureSpeechReactSample/tree/main
@@ -114,15 +114,44 @@ export default function SpeechToTextInput({onRecognizedText}: Props) {
         setIsStoppingRecognition(false);
     }
 
+    // We use an AlertDialog instead of a regular one because the AlertDialog is modal.
+    // We can't make a Dialog modal (unless we use a primitive which is too much work).
     return (
-        <Flex gap="3">
-            <IconButton
-                variant="outline"
-                onClick={() => isRecognizing ? stopContinuousSttFromMic() : startContinuousSttFromMic()}
-                loading={isStoppingRecognition}
-            >
-                <i className={isRecognizing ? "fa-solid fa-microphone-slash" : "fa-solid fa-microphone"}></i>
-            </IconButton>
-        </Flex>
+        <AlertDialog.Root
+            open={isRecognizing}
+            onOpenChange={() => isRecognizing ? stopContinuousSttFromMic() : startContinuousSttFromMic()}
+        >
+            <AlertDialog.Trigger>
+                <IconButton
+                    variant="outline"
+                    loading={isStoppingRecognition}
+                >
+                    <i className="fa-solid fa-microphone"></i>
+                </IconButton>
+            </AlertDialog.Trigger>
+
+            <AlertDialog.Content>
+                <AlertDialog.Title>Speech to Text</AlertDialog.Title>
+                <AlertDialog.Description>What are you in the mood for?</AlertDialog.Description>
+                <Flex direction="column" m="4" justify="center" align="center" minHeight="200px">
+                    <i className={`fa-solid fa-microphone ${isStoppingRecognition ? "" : "fa-fade"} fa-2xl`}></i>
+                    <Text as="p" mt="5" align="center">
+                        {isStoppingRecognition ? "Transcribing..." : "Listening..."}
+                    </Text>
+                </Flex>
+                <Flex direction="row" justify="end" gap="3">
+                    <AlertDialog.Cancel>
+                        <Button color="gray" variant="soft">
+                            Cancel
+                        </Button>
+                    </AlertDialog.Cancel>
+                    <AlertDialog.Action>
+                        <Button loading={isStoppingRecognition}>
+                            Done Speaking
+                        </Button>
+                    </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
     )
 }
