4bca868320eacaeffb00224deb8ce82f3336d034
Brett Abraham <babraham341@gmail.com>
2025-11-21 10:12:46 -0500

    Setup audio analyzer from microphone output

--- src/components/messaging/RecordingIndicator.tsx
+++ src/components/messaging/RecordingIndicator.tsx
@@ -1,7 +1,36 @@
 ﻿import {Flex, Text} from "@radix-ui/themes";
+import {useEffect} from "react";
 
 
 export default function RecordingIndicator() {
+    useEffect(() => {
+        startAudioLevelMonitoring();
+    }, []);
+
+    async function startAudioLevelMonitoring() {
+        try {
+            const stream: MediaStream = await navigator.mediaDevices.getUserMedia({audio: true});
+            const audioContext: AudioContext = new AudioContext();
+            const analyser: AnalyserNode = audioContext.createAnalyser();
+            const microphone: MediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
+
+            analyser.fftSize = 256;
+            microphone.connect(analyser);
+
+            console.log(`Active Audio tracks: ${stream.getAudioTracks().length}`);
+            console.log(`Main Track State: ${stream.getAudioTracks()[0]?.readyState}`);
+            console.log(`Audio Context State: ${audioContext.state}`);
+            console.log(`Audio Context Sample Rate: ${audioContext.sampleRate}`);
+            console.log(`Analyzer FftSize: ${analyser.fftSize}`);
+            console.log(`Analyzer frequencyBinCount: ${analyser.frequencyBinCount}`);
+            console.log(`Analyzer Inputs: ${analyser.numberOfInputs}`);
+            console.log(`Microphone Outputs: ${microphone.numberOfOutputs}`);
+
+        } catch (error) {
+            console.error("Error accessing microphone for audio level monitoring:", error);
+        }
+    }
+
     return (
         <Flex direction="column" m="4" justify="center" align="center" minHeight="200px">
             <i className={`fa-solid fa-microphone fa-fade fa-2xl`}></i>
