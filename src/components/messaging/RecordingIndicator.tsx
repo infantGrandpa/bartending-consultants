80bba0f15adbc30e3a516874107c5559d289ff45
Brett Abraham <babraham341@gmail.com>
2025-11-23 21:20:29 -0500

    Use an abort controller to handle strict mode's remounting

--- src/components/messaging/RecordingIndicator.tsx
+++ src/components/messaging/RecordingIndicator.tsx
@@ -7,36 +7,49 @@ export default function RecordingIndicator() {
     const audioContextRef: RefObject<AudioContext | null> = useRef(null);
 
     useEffect(() => {
+        const abortController = new AbortController();
+
+        async function startAudioLevelMonitoring() {
+            try {
+                const newStream: MediaStream = await navigator.mediaDevices.getUserMedia({audio: true});
+
+                // We need to handle the double mounting of this component when is Strict Mode.
+                // Strict Mode's second mounting overwrites streamRef, meaning we never stop all the tracks on it.
+                // Without this, the browser will think we never stopped recording while is strict mode.
+                if (abortController.signal.aborted) {
+                    stopTracksOnStream(newStream);
+                    return;
+                }
+
+                streamRef.current = newStream;
+                streamRef.current?.getAudioTracks().forEach((track, index) => {
+                    console.log(`Track ${index}: ${track.label}`);
+                })
+
+                audioContextRef.current = new AudioContext();
+                console.log(`Sample Rate: ${audioContextRef.current?.sampleRate}`);
+
+                const analyser: AnalyserNode = audioContextRef.current.createAnalyser();
+                console.log(`Fft Size: ${analyser.fftSize}`);
+
+                const microphone = audioContextRef.current.createMediaStreamSource(streamRef.current);
+                console.log(microphone.numberOfOutputs);
+            } catch (error) {
+                console.error("Error accessing microphone for audio level monitoring:", error);
+            }
+        }
+
         startAudioLevelMonitoring();
 
         return () => {
+            abortController.abort();
+
             if (streamRef.current) {
                 stopTracksOnStream(streamRef.current);
             }
         };
     }, []);
 
-    async function startAudioLevelMonitoring() {
-        try {
-            streamRef.current = await navigator.mediaDevices.getUserMedia({audio: true});
-            streamRef.current?.getAudioTracks().forEach((track, index) => {
-                console.log(`Track ${index}: ${track.label}`);
-            })
-
-            audioContextRef.current = new AudioContext();
-            console.log(`Sample Rate: ${audioContextRef.current?.sampleRate}`);
-
-            const analyser: AnalyserNode = audioContextRef.current.createAnalyser();
-            console.log(`Fft Size: ${analyser.fftSize}`);
-
-            const microphone = audioContextRef.current.createMediaStreamSource(streamRef.current);
-            console.log(microphone.numberOfOutputs);
-
-
-        } catch (error) {
-            console.error("Error accessing microphone for audio level monitoring:", error);
-        }
-    }
 
     function stopTracksOnStream(mediaStream: MediaStream) {
         mediaStream.getTracks().forEach(
