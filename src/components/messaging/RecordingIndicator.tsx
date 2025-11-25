88e842e23af447105bac2c7497700e3f496480b3
Brett Abraham <babraham341@gmail.com>
2025-11-25 08:06:18 -0500

    Use waveform data to more accurate get loudness

--- src/components/messaging/RecordingIndicator.tsx
+++ src/components/messaging/RecordingIndicator.tsx
@@ -32,17 +32,43 @@ export default function RecordingIndicator() {
                 microphone.connect(analyser);
 
                 const bufferLength = analyser.frequencyBinCount;
-                const frequencyDataArray = new Uint8Array(bufferLength);
+                const dataArray = new Uint8Array(bufferLength);
 
                 const updateAudioLevels = () => {
-                    analyser.getByteFrequencyData(frequencyDataArray);
-                    const averageAudioLevel: number = frequencyDataArray.reduce((sum, value) => sum + value, 0) / frequencyDataArray.length;
-                    setAudioLevel(averageAudioLevel);
+                    analyser.getByteTimeDomainData(dataArray);      // This gets the current waveform
+
+                    /*
+                    Calculate Root Mean Square (RMS) of the waveform
+                    We use RMS because it gives a very accurate measurement of perceived loudness.
+
+                    Values are centered around 128, where 128 represents the 0 on a waveform.
+                    Both negative (below 128) and positive (above 128)  values represent "loudness."
+                    Those values would cancel out in an average.
+
+                    For example, 64 and 192 are part of the same loud sound wave.
+                    Averaging them out would get us 128, which would inaccurately imply silence.
+
+                    So we normalize the values to be centered around 0 instead of 128.
+                    Then we square them so positives and negatives don't cancel out.
+                    Then we get the mean of those squared values, then get the square root of the mean.
+
+                    Alternatively, we could normalize and then get the absolute values, but that is less accurate.
+                     */
+                    const sumOfSquares: number = dataArray.reduce((sum, value) => {
+                        const centeredValue: number = (value - 128) / 128; // Center around 0, range -1 to 1
+                        return sum + (centeredValue * centeredValue);
+                    }, 0);
+
+                    // Normalize into a percentage
+                    const rms: number = Math.sqrt(sumOfSquares / dataArray.length);
+                    const normalizedLevels: number = rms * 100;
+
+                    setAudioLevel(normalizedLevels);
                 }
 
                 window.setInterval(updateAudioLevels, 100);
 
-                console.log(frequencyDataArray.length)
+                console.log(dataArray.length)
 
             } catch (error) {
                 console.error("Error accessing microphone for audio level monitoring:", error);
