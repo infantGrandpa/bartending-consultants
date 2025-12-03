ab798e3454b4ee62d96be61b4839d4de48f87f23
Brett Abraham <babraham341@gmail.com>
2025-12-03 13:53:49 -0500

    Allow selection for each bartender

--- src/components/SelectionPage.tsx
+++ src/components/SelectionPage.tsx
@@ -1,14 +1,28 @@
-﻿import {Flex} from "@radix-ui/themes";
-import {bartenders} from "../types/bartenders.ts";
+﻿import {Button, Flex, Grid} from "@radix-ui/themes";
+import {type Bartender, bartenders} from "../types/bartenders.ts";
 import BartenderCard from "./bartenders/BartenderCard.tsx";
+import {useBartender} from "../providers/BartenderProvider.tsx";
+import {useNavigate} from "react-router";
 
 export default function SelectionPage() {
-    return (
-        <Flex>
-            {Object.values(bartenders).map((thisBartender) => (
-                <BartenderCard key={thisBartender.key} bartender={thisBartender}></BartenderCard>
-            ))}
+    const {setSelectedBartender} = useBartender();
+    let navigate = useNavigate();
 
-        </Flex>
+    const bartenderArray = Object.values(bartenders);
+
+    const handleSelect = (bartender: Bartender)=> {
+        setSelectedBartender(bartender);
+        navigate("/chat");
+    }
+
+    return (
+        <Grid columns="3" gap="3" width="auto">
+            {bartenderArray.map((thisBartender, index) => (
+                <Flex key={index} direction="column" gap="3">
+                    <BartenderCard key={index} bartender={thisBartender}/>
+                    <Button onClick={() => handleSelect(thisBartender)}>Choose {thisBartender.profile.firstName}</Button>
+                </Flex>
+            ))}
+        </Grid>
     )
 }
