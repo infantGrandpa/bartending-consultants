5d29dcdd7d5e5ffcfea221aa44b7b5b1c304e9c7
Brett Abraham <babraham341@gmail.com>
2025-12-04 15:59:17 -0500

    Scroll to bottom of scroll area on new messages

--- src/components/messaging/MessageLog.tsx
+++ src/components/messaging/MessageLog.tsx
@@ -1,20 +1,27 @@
 ﻿import {Box, Flex, ScrollArea} from "@radix-ui/themes";
 import MessageBubble from "./MessageBubble.tsx";
 import type {Conversation, Message} from "../../types/conversations.ts";
+import {type RefObject, useEffect, useRef} from "react";
 
 interface Props {
     conversation: Conversation
 }
 
 export default function MessageLog({conversation}: Props) {
+    const messagesEndRef: RefObject<HTMLDivElement | null> = useRef<null | HTMLDivElement>(null)
+
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({behavior: "smooth"})
+    }, [conversation.messages]);
     return (
-        <Box my="9" pb="5">
+        <Box my="9">
             <ScrollArea scrollbars="vertical" style={{height: "100%"}}>
                 <Flex direction="column" gap="3">
                     {conversation.messages.map((thisMessage: Message, index) => (
                         <MessageBubble message={thisMessage} key={index}/>
                     ))}
                 </Flex>
+                <Box ref={messagesEndRef} width="100%" pb="5"/>
             </ScrollArea>
         </Box>
     );
