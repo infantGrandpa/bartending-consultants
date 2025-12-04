e32a54d23b1dd86a810965495a8400b17e5dacc2
Brett Abraham <babraham341@gmail.com>
2025-12-04 14:14:41 -0500

    Extract MessageAudioInput.tsx

---
+++ src/components/messaging/MessageAudioInput.tsx
@@ -0,0 +1,16 @@
+﻿import {IconButton} from "@radix-ui/themes";
+
+export default function MessageAudioInput() {
+    return(
+        <IconButton radius="full" variant="solid"
+                    style={{
+                        color: "var(--gray-3)",
+                        width: "64px",
+                        height: "64px"
+                    }}
+                    onClick={() => console.log("Pretend we sent a message using STT.")}
+        >
+            <i className="fa-solid fa-microphone fa-xl"></i>
+        </IconButton>
+    )
+}
--- src/components/messaging/MessagingControls.tsx
+++ src/components/messaging/MessagingControls.tsx
@@ -2,6 +2,7 @@
 import {useState} from "react";
 import MessageTextInput from "./MessageTextInput.tsx";
 import Settings from "../settings/Settings.tsx";
+import MessageAudioInput from "./MessageAudioInput.tsx";
 
 
 export default function MessagingControls() {
@@ -18,18 +19,8 @@ export default function MessagingControls() {
             />}
 
             <Flex direction="row" justify="between" align="end" width="100%">
-                <Settings />
-                {useMicrophone &&
-                    <IconButton radius="full" variant="solid"
-                                style={{
-                                    color: "var(--gray-3)",
-                                    ...(useMicrophone && {width: "64px", height: "64px"})
-                                }}
-                                onClick={() => console.log("Pretend we sent a message using STT.")}
-                    >
-                        <i className="fa-solid fa-microphone fa-xl"></i>
-                    </IconButton>
-                }
+                <Settings/>
+                {useMicrophone && <MessageAudioInput />}
 
                 <IconButton onMouseDown={handleToggleInputMode}>
                     <i className={useMicrophone ? 'fa-regular fa-keyboard' : 'fa-solid fa-microphone'}
