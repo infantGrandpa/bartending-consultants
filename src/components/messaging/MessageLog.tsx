25f7ecbd5ebdc9c243b65780d9f9e3c12b3ab16f
Brett Abraham <babraham341@gmail.com>
2025-12-04 15:43:42 -0500

    Expand scroll area to fit content

--- src/components/messaging/MessageLog.tsx
+++ src/components/messaging/MessageLog.tsx
@@ -1,4 +1,4 @@
-﻿import {Flex, ScrollArea} from "@radix-ui/themes";
+﻿import {Box, Flex, ScrollArea} from "@radix-ui/themes";
 import MessageBubble from "./MessageBubble.tsx";
 import type {Conversation, Message} from "../../types/conversations.ts";
 
@@ -6,14 +6,16 @@ interface Props {
     conversation: Conversation
 }
 
-export default function MessageLog({conversation}:Props) {
+export default function MessageLog({conversation}: Props) {
     return (
-        <ScrollArea scrollbars="vertical" mt="9" style={{height: "700px"}}>
-            <Flex direction="column" gap="3">
-                {conversation.messages.map((thisMessage: Message, index) => (
-                    <MessageBubble message={thisMessage} key={index}/>
-                ))}
-            </Flex>
-        </ScrollArea>
+        <Box my="9" pb="5">
+            <ScrollArea scrollbars="vertical" style={{height: "100%"}}>
+                <Flex direction="column" gap="3">
+                    {conversation.messages.map((thisMessage: Message, index) => (
+                        <MessageBubble message={thisMessage} key={index}/>
+                    ))}
+                </Flex>
+            </ScrollArea>
+        </Box>
     );
 }
