9e09d88bdb8a67bd0c1cc594149122e4f7357ca3
Brett Abraham <babraham341@gmail.com>
2025-12-04 15:11:40 -0500

    Set Header to fixed position at top

--- src/components/messaging/MessageHeader.tsx
+++ src/components/messaging/MessageHeader.tsx
@@ -1,4 +1,4 @@
-﻿import {Card, Flex, Heading, IconButton} from "@radix-ui/themes";
+﻿import {Box, Card, Flex, Heading, IconButton} from "@radix-ui/themes";
 import {useNavigate} from "react-router";
 import {useBartender} from "../../providers/BartenderProvider.tsx";
 
@@ -16,18 +16,20 @@ export default function MessageHeader({headerText}: Props) {
     }
 
     return (
-        <Card style={{padding: "8px"}}>
-            <Flex direction="row" justify="between" align="center">
-                <IconButton onClick={handleReturn} variant="ghost" style={{paddingLeft: "8px"}}>
-                    <i className="fa-solid fa-chevron-left"></i>
-                </IconButton>
-                <Heading as="h1" size="5" style={{lineHeight: "0"}}>
-                    {headerText}
-                </Heading>
-                <IconButton variant="ghost">
-                    <i className="fa-solid fa-martini-glass"></i>
-                </IconButton>
-            </Flex>
-        </Card>
+        <Box position="fixed" p="3" top="0" left="0" width="100%" style={{zIndex: "1"}}>
+            <Card>
+                <Flex direction="row" justify="between" align="center">
+                    <IconButton onClick={handleReturn} variant="ghost" style={{paddingLeft: "8px"}}>
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </IconButton>
+                    <Heading as="h1" size="5" style={{lineHeight: "0"}}>
+                        {headerText}
+                    </Heading>
+                    <IconButton variant="ghost">
+                        <i className="fa-solid fa-martini-glass"></i>
+                    </IconButton>
+                </Flex>
+            </Card>
+        </Box>
     );
 }
