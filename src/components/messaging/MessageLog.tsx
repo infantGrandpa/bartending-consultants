735efe3a02d0f1e35c627fad2d1ff2f74f7bab8f
Brett Abraham <babraham341@gmail.com>
2025-12-04 15:18:27 -0500

    Remove todo

--- src/components/messaging/MessageLog.tsx
+++ src/components/messaging/MessageLog.tsx
@@ -7,10 +7,6 @@ interface Props {
 }
 
 export default function MessageLog({conversation}:Props) {
-    //TODO: Change this scroll area to be the entire page; everything else should be static
-    // This way we don't have to deal with the weirdness of 2 scrollbars when things go wrong.
-    // For example, the Messages themselves would be scrollable, but the Input would be sticky at the bottom.
-    // Take inspo from ChatGPT and Claude
     return (
         <ScrollArea scrollbars="vertical" mt="9" style={{height: "700px"}}>
             <Flex direction="column" gap="3">
