dd8233f5a998b8c685afd0209f1db1210cc366fa
Brett Abraham <babraham341@gmail.com>
2025-12-04 14:14:59 -0500

    Format MessagingControls.tsx

--- src/components/messaging/MessagingControls.tsx
+++ src/components/messaging/MessagingControls.tsx
@@ -14,13 +14,15 @@ export default function MessagingControls() {
 
     return (
         <Flex position="fixed" gap="2" p="3" bottom="0" left="0" width="100%" direction="column">
-            {!useMicrophone && <MessageTextInput
-                onSendMessage={async () => console.log("Message Sent")}
-            />}
+            {!useMicrophone &&
+                <MessageTextInput
+                    onSendMessage={async () => console.log("Message Sent")}
+                />
+            }
 
             <Flex direction="row" justify="between" align="end" width="100%">
                 <Settings/>
-                {useMicrophone && <MessageAudioInput />}
+                {useMicrophone && <MessageAudioInput/>}
 
                 <IconButton onMouseDown={handleToggleInputMode}>
                     <i className={useMicrophone ? 'fa-regular fa-keyboard' : 'fa-solid fa-microphone'}
