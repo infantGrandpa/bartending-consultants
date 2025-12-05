6dc4b7bf78a14afe56f264110608c3decf7a4fd5
Brett Abraham <babraham341@gmail.com>
2025-12-05 11:19:47 -0500

    Open/Close sidebar

--- src/components/messaging/MessagingPanel.tsx
+++ src/components/messaging/MessagingPanel.tsx
@@ -14,6 +14,7 @@ import MessagingControls from "./MessagingControls.tsx";
 import MessageLog from "./MessageLog.tsx";
 import {useMessageSidebar} from "../../providers/MessageSidebarProvider.tsx";
 import {Text} from "@radix-ui/themes";
+import MessageSidebar from "./MessageSidebar.tsx";
 
 
 export default function MessagingPanel() {
@@ -99,6 +100,7 @@ export default function MessagingPanel() {
             <MessageHeader headerText={selectedBartender ? selectedBartender.profile.displayName : "Nickname"}/>
             <MessageLog conversation={conversation}/>
             <Text>Sidebar Value: {String(isSidebarOpen)}</Text>
+            {isSidebarOpen && <MessageSidebar/>}
             <MessagingControls onSendMessage={handleSendMessage}/>
         </>
     );
