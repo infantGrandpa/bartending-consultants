0e7e1438c04e02edc129c98667a05870fb72f845
Brett Abraham <babraham341@gmail.com>
2025-12-06 16:57:17 -0500

    Add header to SetupPage.tsx

--- src/pages/SetupPage.tsx
+++ src/pages/SetupPage.tsx
@@ -1,5 +1,15 @@
 ﻿import ApiKeysSetup from "../components/settings/ApiKeysSetup.tsx";
+import {Box} from "@radix-ui/themes";
+import Header from "../components/blocks/Header.tsx";
 
 export default function SetupPage() {
-    return <ApiKeysSetup/>
+    return (
+        <>
+            <Header headerText="Enter API Keys"/>
+            <Box pt="9">
+                <ApiKeysSetup/>
+            </Box>
+        </>
+
+    )
 }
