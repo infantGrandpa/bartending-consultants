b4fa551b660dfbe656cd0488338cf38a87dc2242
Brett Abraham <babraham341@gmail.com>
2025-12-06 09:11:23 -0500

    Cycle through drinks in sidebar

--- src/components/messaging/MessageSidebar.tsx
+++ src/components/messaging/MessageSidebar.tsx
@@ -2,11 +2,14 @@
 import DrinkDetails from "../DrinkDetails.tsx";
 import {useMessageSidebar} from "../../providers/MessageSidebarProvider.tsx";
 import {useConversation} from "../../providers/ConversationProvider.tsx";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import type {Drink} from "../../types/drinks.ts";
 
 export default function MessageSidebar() {
     const {isSidebarOpen, openSidebar, closeSidebar, currentDrink, setCurrentDrink} = useMessageSidebar();
-    const {getMostRecentDrink} = useConversation();
+    const {conversation, getMostRecentDrink, getAllDrinks} = useConversation();
+    const [isFirstDrink, setIsFirstDrink] = useState<boolean>();
+    const [isLastDrink, setIsLastDrink] = useState<boolean>();
 
     useEffect(() => {
         if (currentDrink) {
@@ -15,6 +18,31 @@ export default function MessageSidebar() {
 
         setCurrentDrink(getMostRecentDrink())
     }, []);
+
+    useEffect(() => {
+        const allDrinks: Drink[] = getAllDrinks();
+        const currentDrinkIndex: number = allDrinks.findIndex((drink: Drink) => drink === currentDrink);
+        setIsFirstDrink(currentDrinkIndex === 0);
+        setIsLastDrink(currentDrinkIndex === allDrinks.length - 1 || currentDrinkIndex === -1);
+    }, [conversation, currentDrink]);
+
+    const handlePreviousDrink = () => {
+        const allDrinks: Drink[] = getAllDrinks();
+        const currentDrinkIndex: number = allDrinks.findIndex((drink: Drink) => drink === currentDrink);
+
+        if (currentDrinkIndex > 0) {
+            setCurrentDrink(allDrinks[currentDrinkIndex - 1]);
+        }
+    }
+
+    const handleNextDrink = () => {
+        const allDrinks: Drink[] = getAllDrinks();
+        const currentDrinkIndex: number = allDrinks.findIndex((drink: Drink) => drink === currentDrink);
+
+        if (currentDrinkIndex !== -1 && currentDrinkIndex < allDrinks.length - 1) {
+            setCurrentDrink(allDrinks[currentDrinkIndex + 1]);
+        }
+    }
 
     return (
         <Dialog.Root open={isSidebarOpen}>
@@ -49,10 +77,10 @@ export default function MessageSidebar() {
                         </IconButton>
                     </Dialog.Close>
                     <Flex direction="row" gap="2">
-                        <IconButton size="1" variant="soft">
+                        <IconButton size="1" variant="soft" onClick={handlePreviousDrink}  disabled={isFirstDrink}>
                             <i className="fa-solid fa-chevron-left"></i>
                         </IconButton>
-                        <IconButton size="1" variant="soft">
+                        <IconButton size="1" variant="soft" onClick={handleNextDrink} disabled={isLastDrink}>
                             <i className="fa-solid fa-chevron-right"></i>
                         </IconButton>
                     </Flex>
