c5e5e72aedc87b3d1f3f5fbd70493b869aa66920
Brett Abraham <babraham341@gmail.com>
2025-12-07 11:34:34 -0500

    Add popover to signify the drink was copied

--- src/components/CopyDrinkButton.tsx
+++ src/components/CopyDrinkButton.tsx
@@ -1,22 +1,37 @@
-﻿import {IconButton} from "@radix-ui/themes";
+﻿import {IconButton, Popover, Text} from "@radix-ui/themes";
 import {copyDrinkToClipboard, type Drink} from "../types/drinks.ts";
+import {useState} from "react";
 
 interface Props {
     drink: Drink | undefined;
 }
 
 export default function CopyDrinkButton({drink}: Props) {
+    const [drinkWasCopied, setDrinkWasCopied] = useState<boolean>(false);
+
     const handleCopyDrink = async () => {
         if (!drink) {
             return;
         }
         const output = await copyDrinkToClipboard(drink);
         console.log(output)
+        setDrinkWasCopied(true);
+
+        setTimeout(() => {
+            setDrinkWasCopied(false);
+        }, 1500)
     }
 
     return (
-        <IconButton variant="ghost" disabled={!drink} onMouseDown={handleCopyDrink}>
-            <i className="fa-solid fa-clone"></i>
-        </IconButton>
+        <Popover.Root open={drinkWasCopied}>
+            <Popover.Trigger>
+                <IconButton variant="ghost" disabled={!drink} onMouseDown={handleCopyDrink}>
+                    <i className="fa-solid fa-clone"></i>
+                </IconButton>
+            </Popover.Trigger>
+            <Popover.Content size="1">
+                <Text as="p" size="1">Copied to Clipboard!</Text>
+            </Popover.Content>
+        </Popover.Root>
     )
 }
