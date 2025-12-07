f83d2a727dfcbc263cc95eeecfbfecaf85b03780
Brett Abraham <babraham341@gmail.com>
2025-12-07 11:47:34 -0500

    Get most recent drink when opening the sidebar from the header

--- src/components/messaging/MessageSidebar.tsx
+++ src/components/messaging/MessageSidebar.tsx
@@ -18,7 +18,7 @@ export default function MessageSidebar() {
         }
 
         setCurrentDrink(getMostRecentDrink())
-    }, []);
+    }, [isSidebarOpen]);
 
     useEffect(() => {
         const allDrinks: Drink[] = getAllDrinks();
