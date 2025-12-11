3acfe613b7d3024ae7282465eda193be8b918ce3
Brett Abraham <babraham341@gmail.com>
2025-12-11 16:50:37 -0500

    Edit user prompts for clarity

--- src/types/userPrompts.ts
+++ src/types/userPrompts.ts
@@ -1,13 +1,12 @@
 ﻿export const genericPrompts: string[] = [
     "What spirits do you like?",
-    "Do you like smokey, sour, fruity, or something else?",
-    "Do you want a light drink, something balanced, or something spirit-forward?",
+    "Do you like smokey, sour, fruity, or other flavors?",
+    "Do you want something light, balanced, or spirit-forward?",
     "What kind of mood are you in tonight?",
     "Are you looking for something new or familiar?",
     "Tell me about the last drink you loved.",
     "What flavors do you hate?"
 ]
-
 
 export function getRandomUserPrompts(promptsToGet: number): string[] {
     const shuffledPrompts: string[] = [...genericPrompts].sort(() => Math.random() - 0.5);
