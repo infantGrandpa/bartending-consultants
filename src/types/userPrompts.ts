71afc237f5737f26b3c71c81ff963f9d6123b708
Brett Abraham <babraham341@gmail.com>
2025-12-11 14:44:35 -0500

    Create generic prompts

---
+++ src/types/userPrompts.ts
@@ -0,0 +1,14 @@
+﻿export const genericPrompts: string[] = [
+    "What spirits do you like?",
+    "Do you like smokey, sour, fruity, or something else?",
+    "Do you want a light drink, something balanced, or something spirit-forward?",
+    "What kind of mood are you in tonight?",
+    "Are you looking for something new or familiar?",
+    "Tell me about the last drink you loved.",
+    "What flavors do you hate?"
+]
+
+
+export function getBartenderPrompts(): string[] {
+    return genericPrompts;
+}
